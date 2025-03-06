2eaa510f7c131fc6a40228afa7b40e2535fc66aa
Rodrigo Calero <moonb@DESKTOP-F13L1RU.>
2025-03-06 16:44:39 +0100

    Updated client image

--- images/client/entrypoint.sh
+++ images/client/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-if [ "$AUTO_RUN" = "true" ]; then
-    /usr/local/bin/behavior.sh &
-fi
+: ${SLEEP_TIME:=5}
+: ${REQUEST_IP:="0.0.0.0"}
+: ${AUTORUN:="true"}
 
-tail -f /dev/null
+while [ "$AUTORUN" = "true" ]; do
+    /behavior/behavior.sh
+    sleep $SLEEP_TIME
+
+    AUTORUN=$(printenv AUTORUN)
+done
