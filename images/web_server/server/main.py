a6cf42e75dafe7249727f410da0d1f0ae1c972d0
Rodrigo Calero <r.calero.albarran@gmail.com>
2025-03-12 18:38:01 +0100

    Added HTTP methods to web server

--- images/web_server/server/main.py
+++ images/web_server/server/main.py
@@ -3,5 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def root():
-    return {"message": "Lechuga"}
+def root_get():
+    return {"message": "Hello from the GET method!"}
+
+@app.post("/")
+def root_post():
+    return {"message": "Hello from the POST method!"}
+
+@app.put("/")
+def root_put():
+    return {"message": "Hello from the PUT method!"}
+
+@app.delete("/")
+def root_delete():
+    return {"message": "Hello from the DELETE method!"}
